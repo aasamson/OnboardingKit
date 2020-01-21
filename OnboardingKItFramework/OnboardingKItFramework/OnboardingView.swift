edd13af991df39afc9ffe170bb58b88c76eb1d9e
Aira Samson <aira.samson.a@gmail.com>
2020-01-21 10:38:49 +0800

    added prev and next button and functionality to scroll and tapped

--- OnboardingKItFramework/OnboardingKItFramework/OnboardingView.swift
+++ OnboardingKItFramework/OnboardingKItFramework/OnboardingView.swift
@@ -14,6 +14,8 @@ open class OnboardingView : UIView {
     
     var onboardingImages : [String] =  ["1.jpg","2.jpg","3.jpg"]
     
+    var numberOfPages: Int?
+    
     lazy var collection: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
@@ -34,22 +36,81 @@ open class OnboardingView : UIView {
         pageControl.currentPageIndicatorTintColor = .orange
         pageControl.pageIndicatorTintColor = .lightGray
         pageControl.backgroundColor = .white
+        pageControl.currentPage = 0
+        pageControl.numberOfPages = onboardingImages.count
         pageControl.translatesAutoresizingMaskIntoConstraints = false
+        pageControl.addTarget(self, action: #selector(pageControltapped(_:)), for: .touchUpInside)
         return pageControl
     }()
+    
+    @objc func pageControltapped(_ sender: Any) {
+        let page: Int? = pageControl.currentPage
+        var frame: CGRect = collection.frame
+        frame.origin.x = frame.size.width * CGFloat(page ?? 0)
+        frame.origin.y = 0
+        collection.scrollRectToVisible(frame, animated: true)
+    }
+    
+    lazy var nxtButton : UIButton = {
+        let nextButton = UIButton()
+        nextButton.translatesAutoresizingMaskIntoConstraints = false
+        nextButton.setTitle("Next", for: .normal)
+        nextButton.setTitleColor(UIColor.darkGray, for: .normal)
+        nextButton.addTarget(self, action: #selector(didTapNext), for: .touchUpInside)
+        return nextButton
+    }()
+    
+    @objc func didTapNext(_ sender: Any) {
+        if self.pageControl.currentPage != self.numberOfPages {
+            collection.scrollToItem(at: IndexPath(row: pageControl.currentPage + 1, section: 0), at: .right, animated: true)
+            nxtButton.setTitle("Next", for: .normal)
+        } else {
+            nxtButton.setTitle("Start", for: .normal)
+            nxtButton.setTitleColor(UIColor.orange, for: .normal)
+        }
+        
+    }
+    
+    lazy var prevButton : UIButton = {
+        let prevButton = UIButton()
+        prevButton.translatesAutoresizingMaskIntoConstraints = false
+        prevButton.setTitleColor(UIColor.darkGray, for: .normal)
+        prevButton.addTarget(self, action: #selector(didTapPrev), for: .touchUpInside)
+        return prevButton
+    }()
+
+    @objc func didTapPrev(_ sender: Any) {
+         
+        if pageControl.currentPage == 0 {
+            
+            prevButton.setTitle("", for: .normal)
+            
+        } else {
+            
+            collection.scrollToItem(at: IndexPath(row: pageControl.currentPage - 1, section: 0), at: .left, animated: true)
+            prevButton.setTitle("Prev", for: .normal)
+            print("here")
+        }
+        
+    }
     
     override public init(frame: CGRect) {
         super.init(frame: frame)
         
-        self.addSubview(collection)
-        self.addSubview(pageControl)
-        
         collection.delegate = self
         collection.dataSource = self
         
+        self.addSubview(collection)
+        self.addSubview(pageControl)
+        self.addSubview(nxtButton)
+        self.addSubview(prevButton)
+    
         pageControl.numberOfPages = onboardingImages.count
+        self.numberOfPages = pageControl.numberOfPages - 1
         
+
         setupAutoLayout()
+        
     }
     
     required public init?(coder: NSCoder) {
@@ -73,7 +134,16 @@ open class OnboardingView : UIView {
             pageControl.topAnchor.constraint(equalTo: collection.bottomAnchor),
             pageControl.rightAnchor.constraint(equalTo: self.rightAnchor),
             pageControl.leftAnchor.constraint(equalTo: self.leftAnchor),
-            pageControl.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+            pageControl.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            nxtButton.topAnchor.constraint(equalTo: collection.bottomAnchor),
+            nxtButton.rightAnchor.constraint(equalTo: self.rightAnchor),
+            nxtButton.leftAnchor.constraint(equalTo: pageControl.rightAnchor, constant: -100),
+            nxtButton.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            prevButton.topAnchor.constraint(equalTo: collection.bottomAnchor),
+            prevButton.rightAnchor.constraint(equalTo: pageControl.leftAnchor,constant: 100),
+            prevButton.leftAnchor.constraint(equalTo: self.leftAnchor),
+            prevButton.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            
         ])
         
     }
@@ -89,7 +159,8 @@ extension OnboardingView : UICollectionViewDataSource, UICollectionViewDelegate,
     
     public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
-        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
+        //return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
+        return collection.bounds.size
     }
     
     public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -98,7 +169,7 @@ extension OnboardingView : UICollectionViewDataSource, UICollectionViewDelegate,
     
         cell.image.image = UIImage(named: onboardingImages[indexPath.row])
         
-        print("cell")
+        
         
         return cell
     }
@@ -108,6 +179,30 @@ extension OnboardingView : UICollectionViewDataSource, UICollectionViewDelegate,
         let index = scrollView.contentOffset.x / witdh
         let roundedIndex = round(index)
         pageControl.currentPage = Int(roundedIndex)
+        if pageControl.currentPage == self.numberOfPages {
+            nxtButton.setTitle("Start", for: .normal)
+            nxtButton.setTitleColor(UIColor.orange, for: .normal)
+            prevButton.setTitle("Prev", for: .normal)
+            prevButton.setTitleColor(UIColor.darkGray, for: .normal)
+        } else if pageControl.currentPage == 0 {
+            nxtButton.setTitle("Next", for: .normal)
+            nxtButton.setTitleColor(UIColor.darkGray, for: .normal)
+            prevButton.setTitle("", for: .normal)
+            prevButton.setTitleColor(UIColor.darkGray, for: .normal)
+        } else {
+            nxtButton.setTitle("Next", for: .normal)
+            nxtButton.setTitleColor(UIColor.darkGray, for: .normal)
+            prevButton.setTitle("Prev", for: .normal)
+            prevButton.setTitleColor(UIColor.darkGray, for: .normal)
+        }
     }
+    
+    /*public func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+       let visibleRect = CGRect(origin: collection.contentOffset, size: collection.bounds.size)
+       let visiblePoint = CGPoint(x: visibleRect.midX, y: visibleRect.midY)
+       if let visibleIndexPath = collection.indexPathForItem(at: visiblePoint) {
+                pageControl.currentPage = visibleIndexPath.row
+       }
+    }*/
     
 }
